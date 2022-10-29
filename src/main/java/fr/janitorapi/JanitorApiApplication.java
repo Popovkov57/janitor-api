73d04bc72cef833a902ace593682e4c34c7a5721
Popovkov57 <kevin.patrzek@live.fr>
2022-10-30 00:29:14 +0200

    Init project with Spring Boot

---
+++ src/main/java/fr/janitorapi/JanitorApiApplication.java
@@ -0,0 +1,13 @@
+package fr.janitorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanitorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/janitorapi/JanitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.janitorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
